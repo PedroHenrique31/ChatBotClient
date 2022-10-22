d8dcbfeace705902d3c337120a4c36dafe60beff
Pedro Henrique Carneiro de Araújo <pedrohcaraujo1@hotmail.com>
2022-10-21 22:10:00 -0300

    Commit inicial do projeto do lado do cliente.

---
+++ AplicacaoCliente/Cliente.java
@@ -0,0 +1,35 @@
+package AplicacaoCliente;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Cliente  {
+
+	public static void main(String[] args) {
+		Socket socketCliente;
+		Scanner leitorTeclado;
+		PrintStream mensagemEnviar=null;
+		String textoEnviar;
+		int porta=8080;
+		leitorTeclado=new Scanner(System.in);
+		
+		try {
+			socketCliente=new Socket("localhost",porta);
+			mensagemEnviar=new PrintStream(socketCliente.getOutputStream());
+		}catch(IOException e) {
+			System.out.println("ERRO DE ENTRADA OU SAÍDA!!");
+			e.printStackTrace();
+		}
+		
+		
+		System.out.println("Digite uma palavra para mandarmos no servidor");
+		textoEnviar=leitorTeclado.nextLine();
+		mensagemEnviar.println(textoEnviar);
+		
+		
+		
+	}
+
+}
